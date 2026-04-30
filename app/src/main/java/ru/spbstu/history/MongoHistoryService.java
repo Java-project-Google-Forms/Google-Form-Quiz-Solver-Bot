d4bec6b1e71479e65050aed2e74d38b6c8175c82
vivol <vivoln@mail.ru>
2026-04-30 04:49:34 +0300

    added missing import in MongoHistoryService

--- app/src/main/java/ru/spbstu/history/MongoHistoryService.java
+++ app/src/main/java/ru/spbstu/history/MongoHistoryService.java
@@ -10,6 +10,8 @@ import ru.spbstu.database.repository.FormRepository;
 import ru.spbstu.database.repository.UserRepository;
 import ru.spbstu.messagehandler.service.api.HistoryService;
 
+import java.util.stream.Collectors;
+
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
